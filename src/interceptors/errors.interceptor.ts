0ba19ef34bc4a70ada2ac5ef4db51207341ab72f
emrekaramese <emrekaramese45@gmail.com>
2023-04-09 18:42:32 +0300

    Postman Collection

--- src/interceptors/errors.interceptor.ts
+++ src/interceptors/errors.interceptor.ts
@@ -2,8 +2,9 @@ import {
   Injectable,
   NestInterceptor,
   ExecutionContext,
-  CallHandler, Logger
-} from "@nestjs/common";
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -13,46 +14,32 @@ export class ErrorsInterceptor implements NestInterceptor {
     return next.handle().pipe(
       catchError(async (err) => {
         Logger.error(err);
-        context.switchToHttp()
-          .getResponse()
-          .status(err.status);
+        context.switchToHttp().getResponse().status(err.status);
 
         err.code;
 
-        if(err.response?.message){
-            return {
-                error: {
-                    message: err.response?.message,
-                        name: err.response?.error,
-                        exception: err?.name,
-                },
-                code: err.status,
-                    success: false,
-            }
-        }
-
-        if(err.code && err.code === '23503') {
-            return {
-                error: {
-                    message: err.table,
-                    name: 'foreign_key_violation',
-                    exception: err.name,
-                },
-                code: 409,
-                success: false,
-            };
+        if (err.response?.message) {
+          return {
+            error: {
+              message: err.response?.message,
+              name: err.response?.error,
+              exception: err?.name,
+            },
+            code: err.status,
+            success: false,
+          };
         }
 
         return {
-              error: {
-                message: err.message,
-                name: err.name,
-                exception: err.name,
-              },
-            code: err.status,
-            success: false,
-            };
+          error: {
+            message: err.message,
+            name: err.name,
+            exception: err.name,
+          },
+          code: err.status,
+          success: false,
+        };
       }),
-    )
+    );
   }
 }
--- src/interceptors/transform.interceptor.ts
+++ src/interceptors/transform.interceptor.ts
@@ -4,21 +4,24 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
-import { map, Observable } from "rxjs";
+import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        context.switchToHttp()
+        context
+          .switchToHttp()
           .getResponse()
-          .status(data?.error ? data.code : 201);
-        return data?.error ? data : {
-          data: data,
-          code: 201,
-          success: true,
-        };
+          .status(data?.error ? data.code : 200);
+        return data?.error
+          ? data
+          : {
+              data: data,
+              code: 200,
+              success: true,
+            };
       }),
     );
   }
